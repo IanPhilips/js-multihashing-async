f95e547805b4aa3d0852852cee121afe23212c99
Jack Kleeman <jackkleeman@gmail.com>
2017-03-31 17:18:25 +0100

    use native-crypto library if available

--- src/crypto-sha1-2-browser.js
+++ src/crypto-sha1-2-browser.js
@@ -3,6 +3,7 @@
 const nodeify = require('nodeify')
 
 const webCrypto = getWebCrypto()
+const nCrypto = self.nCrypto
 
 function getWebCrypto () {
   if (self.crypto) {
@@ -16,7 +17,7 @@ function getWebCrypto () {
 
 function webCryptoHash (type) {
   if (!webCrypto) {
-    throw new Error('Please use a browser with webcrypto support')
+    throw new Error('Please use a browser with webcrypto support, or import nCrypto to the global namespace')
   }
 
   return (data, callback) => {
@@ -39,16 +40,31 @@ function webCryptoHash (type) {
   }
 }
 
+function nCryptoHash (type) {
+  if (!nCrypto) {
+    console.log('nCrypto not found by js-multihashing; falling back on webcrypto');
+    return webCryptoHash(type)
+  }
+
+  return (data, callback) => {
+    const hash = new nCrypto.Hash(type.toLowerCase());
+    nodeify(
+      hash.update(data).digest(),
+      callback
+    )
+  }
+}
+
 function sha1 (buf, callback) {
-  webCryptoHash('SHA-1')(buf, callback)
+  nCryptoHash('SHA-1')(buf, callback)
 }
 
 function sha2256 (buf, callback) {
-  webCryptoHash('SHA-256')(buf, callback)
+  nCryptoHash('SHA-256')(buf, callback)
 }
 
 function sha2512 (buf, callback) {
-  webCryptoHash('SHA-512')(buf, callback)
+  nCryptoHash('SHA-512')(buf, callback)
 }
 
 module.exports = {
